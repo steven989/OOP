3187ca4919d75d7be3820e62046c948d3fae50bf
Steve Long <steven989@gmail.com>
2014-03-26 19:21:14 -0400

    Finished exercise 1

---
+++ people.rb
@@ -0,0 +1,50 @@
+class People
+
+  def initialize(str)
+    @name = str
+  end
+
+  def greetings
+    puts "Hi, my name is #{@name}"
+  end
+
+end 
+
+
+
+class Student < People
+
+  def learn
+    puts "I get it!"
+  end
+
+
+end
+
+
+class Instructor < People
+
+  def teach
+    puts "Everything in Ruby is an object"
+  end 
+
+
+end
+
+
+
+chris=Instructor.new("Chris") # create an instructor called Chris
+chris.greetings #call his greeting
+
+
+christina=Student.new("Christina")  #created a student
+christina.greetings #call her greeting
+
+chris.teach
+christina.learn
+
+christina.teach #this leads to an error resulting from the teach method not being defined in the Student class, to which Christina belongs. Christina only has two methods: learning, and geetings (inherited from being part of the People class)
+
+
+
+
